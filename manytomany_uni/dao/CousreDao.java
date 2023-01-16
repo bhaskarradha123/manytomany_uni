e688f4e0dfff394db0b8463d4d06c7f4560109b3
bhaskarradha123 <mailmeasradha@gmail.com>
2023-01-16 10:04:40 +0530

    manytomany_uni

---
+++ manytomany_uni/dao/CousreDao.java
@@ -0,0 +1,81 @@
+package manytomany_uni.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+import manytomany_uni.dto.Course;
+
+
+public class CousreDao {
+	public EntityManager getEntityManager() {
+		EntityManagerFactory entityManagerFactory=Persistence.createEntityManagerFactory("radha");
+		return entityManagerFactory.createEntityManager();
+		
+		
+	}
+	
+	
+	public void addCourse(Course course) {
+		EntityManager entityManager=getEntityManager();
+		EntityTransaction entityTransaction=entityManager.getTransaction();
+		
+		entityTransaction.begin();
+
+		
+		entityManager.persist(course);
+		
+		entityTransaction.commit();
+	}
+	
+	public void updateCourse(int id,Course course) {
+		EntityManager entityManager=getEntityManager();
+		EntityTransaction entityTransaction=entityManager.getTransaction();
+		Course course2=entityManager.find(Course.class, id);
+		if(course2!=null) {
+			course.setId(id);
+			
+			entityTransaction.begin();
+			entityManager.merge(course);
+			entityTransaction.commit();
+			
+		}
+		else
+			System.out.println("Such school is not exist");
+	}
+	
+	public void deleteCourse(int id) {
+		EntityManager entityManager=getEntityManager();
+		EntityTransaction entityTransaction=entityManager.getTransaction();
+		Course course=entityManager.find(Course.class, id);
+		if(course!=null)
+		{
+			entityTransaction.begin();
+			entityManager.remove(course);
+			entityTransaction.commit();
+		}
+		else
+			System.out.println("Not exist the school");
+	}
+	
+	public void getdetails(int id) {
+		EntityManager entityManager=getEntityManager();
+		Course course= entityManager.find(Course.class, id);
+		if(course!=null)
+		System.out.println(course);
+		else
+			System.out.println("No such school");
+	}
+	
+	public void getDetailsAllCourse() {
+		EntityManager entityManager=getEntityManager();
+		Query  query=entityManager.createQuery("select a from Course a");
+		List<Course> course=query.getResultList();
+		System.out.println(course);
+	}
+	
+}
